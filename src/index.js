e0a95359e86ceb7804e6e2b72e3967702bab25ba
Zach Bodensteiner <zach.bodensteiner@gmail.com>
2018-08-07 08:32:42 -0500

    initial Hello World! commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class HelloWorld extends React.Component {
+
+  render() {
+    return (
+      <div>
+          Hello World!
+      </div>
+    );
+  }
+}
+
+
+
+// ========================================
+
+ReactDOM.render(
+<HelloWorld />,
+document.getElementById('root')
+);
